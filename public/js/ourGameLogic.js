f892507b854c43e6c127fdec74a22a341f46d12c
polymaze <polymaze@gmail.com>
2016-02-17 11:46:26 +0000

    presentation getting along

--- public/js/ourGameLogic.js
+++ public/js/ourGameLogic.js
@@ -3,10 +3,11 @@
 // Create the main game div
 /* Have this load canvas elements in 
    .ourGame instead */
+/*
 var section = document.createElement("section");
 section.id = 'ourGame';
 document.body.appendChild(section);
-
+*/
 
 // Create the ourGame canvas layers
 var canvasLayers = ["map", "player"];
@@ -18,13 +19,13 @@ for (var i = 0; i < canvasLayers.length; i++) {
   // Create the canvas
   var canvas = document.createElement("canvas");
   var ctx = canvas.getContext("2d");
-  section = document.getElementById("ourGame");
+  var drawLocation = document.getElementById("ourGame");
   canvas.id = canvasId;
   /* http://stackoverflow.com/questions/2242086/
   how-to-detect-the-screen-resolution-with-javascript */
   // canvas.height = screen.height;
   // canvas.width = screen.width;
-  section.appendChild(canvas);
+  drawLocation.appendChild(canvas);
 }
 
 
