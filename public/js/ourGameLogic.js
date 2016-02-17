88f4480afcb87f576aa3ba581176546ff9e8e213
polymaze <polymaze@gmail.com>
2016-02-17 09:41:27 +0000

    first steps at presentation

--- public/js/ourGameLogic.js
+++ public/js/ourGameLogic.js
@@ -1,17 +1,15 @@
 
+
 // Create the main game div
-var div = document.createElement("div");
-div.style.width = "100px";
-div.style.height = "100px";
-div.style.background = "red";
-// div.style.color = "white";
-// div.innerHTML = "Hello";
-div.id = 'ourGame';
-document.body.appendChild(div);
+/* Have this load canvas elements in 
+   .ourGame instead */
+var section = document.createElement("section");
+section.id = 'ourGame';
+document.body.appendChild(section);
 
 
 // Create the ourGame canvas layers
-var canvasLayers = ["background", "player"];
+var canvasLayers = ["map", "player"];
 
 for (var i = 0; i < canvasLayers.length; i++) {
   // Tag the canvas elements
@@ -20,10 +18,14 @@ for (var i = 0; i < canvasLayers.length; i++) {
   // Create the canvas
   var canvas = document.createElement("canvas");
   var ctx = canvas.getContext("2d");
-  div = document.getElementById("ourGame");
+  section = document.getElementById("ourGame");
   canvas.id = canvasId;
-  // http://stackoverflow.com/questions/2242086/how-to-detect-the-screen-resolution-with-javascript
-  canvas.height = screen.height;
-  canvas.width = screen.width;
-  div.appendChild(canvas);
+  /* http://stackoverflow.com/questions/2242086/
+  how-to-detect-the-screen-resolution-with-javascript */
+  // canvas.height = screen.height;
+  // canvas.width = screen.width;
+  section.appendChild(canvas);
 }
+
+
+// Start the game
