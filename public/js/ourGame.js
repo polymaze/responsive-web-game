d077197fcce336261e72895b510510f66902a67c
polymaze <polymaze@gmail.com>
2016-03-07 04:06:58 +0000

    it's a start

--- public/js/ourGame.js
+++ public/js/ourGame.js
@@ -4,5 +4,54 @@ window.onload = function() {
 };
 
 var ourGame = function() {
-  console.log("Hello World!");
+    
+  // Create the player
+  var player = function() {
+    console.log("player loaded");
+  };
+  
+  // Load the map
+  var map = [
+    [1,1,1,1,1,1,1],
+    [1,0,0,1,0,0,1],
+    [1,0,0,0,0,0,1],
+    [1,0,0,1,0,0,1],
+    [1,1,1,1,1,1,1]
+    ];
+  
+  // Create the board
+  var board = function(location, map) {
+    /*
+      draw the map in this location
+    */
+    
+    console.log("board loaded");
+  };
+  
+  // Reset the game
+  var reset = function() {
+    console.log("game reset");
+  };
+  
+  // Update the game
+  var update = function() {
+    player();
+    board();
+    console.log("game updated");
+  };
+  
+  // Draw the game
+  var draw = function() {
+    console.log("game drawn");
+  };
+  
+  // Game loop
+  var gameLoop = function() {
+    update();
+    draw();
+    console.log("game loop working");
+  };
+  
+  gameLoop();
+  
 };
